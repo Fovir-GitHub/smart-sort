e42b413a175402055f4b81e1ffa2fd0d5e0f30e3
Ding Yanwen <182461378+SylviaDillion@users.noreply.github.com>
2025-12-13 02:13:10 +0800

    style: format code
    Co-authored-by: Fovir <fovir@disroot.org>

--- src/core/sort_algorithm.hpp
+++ src/core/sort_algorithm.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
-#include <functional>
-#include <utility>
-#include <iterator>
 #include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <utility>
+#include <vector>
 
 namespace smart_sort {
 
@@ -27,62 +29,54 @@ void bubbleSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
 
 template <typename Iter, typename Compare = std::less<>>
 void selectionSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
-    if(start == end){
+    if (start == end) {
         return;
     }
+
     for (Iter i = start; i != end; i++) {
         Iter k = i;
         for (Iter j = std::next(i); j != end; j++) {
-            if (cmp(*j, *k))
-                k = j; 
+            if (cmp(*j, *k)) {
+                k = j;
+            }
         }
         std::swap(*i, *k);
     }
 }
 
 template <typename Iter, typename Compare = std::less<>>
-void merge(Iter start, Iter mid, Iter end ,Compare cmp = std::less<>{}) {
-
+void merge(Iter start, Iter mid, Iter end, Compare cmp = std::less<>{}) {
     using ValueType = typename std::iterator_traits<Iter>::value_type;
-
     std::vector<ValueType> left_temp(start, mid);
     std::vector<ValueType> right_temp(mid, end);
-
-    size_t i = 0;         
-    size_t j = 0;         
+    size_t i = 0;
+    size_t j = 0;
     Iter k = start;
-    while (i < left_temp.size() && j < right_temp.size()) {
 
-        if (cmp(right_temp[j], left_temp[i])) { 
-            *k = right_temp[j];
-            j++;
+    while (i < left_temp.size() && j < right_temp.size()) {
+        if (cmp(right_temp[j], left_temp[i])) {
+            *k = right_temp[j++];
         } else {
-          
-            *k = left_temp[i];
-            i++;
+            *k = left_temp[i++];
         }
-        k++; 
-    }
-    while (i < left_temp.size()) {
-        *k = left_temp[i];
-        i++;
         k++;
     }
 
+    while (i < left_temp.size()) {
+        *k++ = left_temp[i++];
+    }
 
     while (j < right_temp.size()) {
-        *k = right_temp[j];
-        j++;
-        k++;
+        *k++ = right_temp[j++];
     }
-   
 }
 
 template <typename Iter, typename Compare = std::less<>>
 void mergeSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
-    if(std::distance(start, end) <= 1){
+    if (std::distance(start, end) <= 1) {
         return;
     }
+
     Iter mid = start;
     std::advance(mid, std::distance(start, end) / 2);
     mergeSort(start, mid, cmp);
@@ -92,55 +86,44 @@ void mergeSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
 
 template <typename Iter, typename Compare = std::less<>>
 Iter partition(Iter start, Iter end, Compare cmp = std::less<>{}) {
-
     if (start >= end) {
-        return start; 
+        return start;
     }
 
     using ValueType = typename std::iterator_traits<Iter>::value_type;
-    ValueType pivot_value = *start; 
-
+    ValueType pivot_value = *start;
     Iter i = start;
     Iter j = end;
 
     while (i != j) {
-
-        while (i != j && !cmp(*j, pivot_value)) { 
+        while (i != j && !cmp(*j, pivot_value)) {
             j--;
         }
-
         while (i != j && !cmp(pivot_value, *i)) {
             i++;
         }
-        
         if (i != j) {
-            std::iter_swap(i, j); 
+            std::iter_swap(i, j);
         }
     }
-
     std::iter_swap(start, i);
-    
-    return i; 
+    return i;
 }
 
 template <typename Iter, typename Compare = std::less<>>
 void quickSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
-    if (std::distance(start, end) <= 1) { 
+    if (std::distance(start, end) <= 1) {
         return;
     }
-    
+
     Iter last_element = std::prev(end);
-
-
     Iter pivot_final_pos = smart_sort::partition(start, last_element, cmp);
-    
+    Iter next_to_pivot = std::next(pivot_final_pos);
 
     quickSort(start, pivot_final_pos, cmp);
-
-    Iter next_to_pivot = std::next(pivot_final_pos);
     quickSort(next_to_pivot, end, cmp);
-    
-}
 }
 
- // namepace smart_sort
+} // namespace smart_sort
+
+// namepace smart_sort
