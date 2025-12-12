5cd70b4f4b285c6aeb7f8318464318f6de822ea1
Ding Yanwen <182461378+SylviaDillion@users.noreply.github.com>
2025-12-13 01:01:30 +0800

    feat: add mergeSort function and merge function

--- src/core/sort_algorithm.hpp
+++ src/core/sort_algorithm.hpp
@@ -38,11 +38,58 @@ void selectionSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
     }
 }
 
+template <typename Iter, typename Compare = std::less<>>
+void merge(Iter start, Iter mid, Iter end ,Compare cmp = std::less<>{}) {
+
+    using ValueType = typename std::iterator_traits<Iter>::value_type;
+
+    std::vector<ValueType> left_temp(start, mid);
+    std::vector<ValueType> right_temp(mid, end);
+
+    size_t i = 0;         
+    size_t j = 0;         
+    Iter k = start;
+    while (i < left_temp.size() && j < right_temp.size()) {
+
+        if (cmp(right_temp[j], left_temp[i])) { 
+            *k = right_temp[j];
+            j++;
+        } else {
+          
+            *k = left_temp[i];
+            i++;
+        }
+        k++; 
+    }
+    while (i < left_temp.size()) {
+        *k = left_temp[i];
+        i++;
+        k++;
+    }
+
+
+    while (j < right_temp.size()) {
+        *k = right_temp[j];
+        j++;
+        k++;
+    }
+   
+}
 
 template <typename Iter, typename Compare = std::less<>>
-void mergeSort(Iter start, Iter end, Compare cmp = std::less<>{}) {}
+void mergeSort(Iter start, Iter end, Compare cmp = std::less<>{}) {
+    if(std::distance(start, end) <= 1){
+        return;
+    }
+    Iter mid = start;
+    std::advance(mid, std::distance(start, end) / 2);
+    mergeSort(start, mid, cmp);
+    mergeSort(mid, end, cmp);
+    merge(start, mid, end, cmp);
+}
 
 
 template <typename Iter, typename Compare = std::less<>>
 void quickSort(Iter start, Iter end, Compare cmp = std::less<>{}) {}
+
 } // namespace smart_sort
