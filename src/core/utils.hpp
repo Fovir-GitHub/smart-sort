03d6bfadcebde2bab3831f7eeb604bc6dbe80d4b
Fovir <fovir@disroot.org>
2025-12-13 15:53:31 +0800

    refactor: move some test functions into `core/utils`

--- src/core/utils.hpp
+++ src/core/utils.hpp
@@ -1,6 +1,18 @@
+#pragma once
+
+#include "sort_algorithm.hpp"
 #include <chrono>
+#include <functional>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace smart_sort {
+
+template <typename TYPE>
+using AlgorithmMap =
+    std::unordered_map<std::string, std::function<void(std::vector<TYPE> &)>>;
 
 const int DEFAULT_RUN_TIMES = 10;
 
@@ -21,5 +33,39 @@ long double measureAverageTime(FUNCTION f, int times = DEFAULT_RUN_TIMES) {
     }
     return result;
 }
+
+/**
+ * @brief Construct an algorithm map with type and compare function provided.
+ *
+ * @tparam TYPE Array type.
+ * @param cmp Compare function.
+ * @return A map contains pairs `<name, sorter>`.
+ */
+template <typename TYPE, typename COMPARE = std::less<>>
+AlgorithmMap<TYPE> getAlgorithmMapByType(COMPARE cmp = std::less<>{}) {
+    // More algorithms goes here:
+    std::unordered_map<std::string, std::function<void(std::vector<TYPE> &)>>
+        result = {
+            {"Bubble Sort",
+             [cmp](std::vector<TYPE> & v) {
+                 smart_sort::bubbleSort(v.begin(), v.end(), cmp);
+             }},
+            {"Selection Sort",
+             [cmp](std::vector<TYPE> & v) {
+                 smart_sort::selectionSort(v.begin(), v.end(), cmp);
+             }},
+            {"Merge Sort",
+             [cmp](std::vector<TYPE> & v) {
+                 smart_sort::mergeSort(v.begin(), v.end(), cmp);
+             }},
+            {"Quick Sort",
+             [cmp](std::vector<TYPE> & v) {
+                 smart_sort::quickSort(v.begin(), v.end(), cmp);
+             }},
+        };
+    return result;
+}
+
+std::mt19937 getRandomGenerator();
 
 } // namespace smart_sort
--- tests/test_sort.cpp
+++ tests/test_sort.cpp
@@ -1,27 +1,15 @@
-#include "core/sort_algorithm.hpp"
+#include "core/utils.hpp"
 #include <algorithm>
 #include <functional>
 #include <gtest/gtest.h>
 #include <random>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 const static int LOWER_BOUNDARY = 1;
 const static int UPPER_BOUNDARY = 100;
 const static int TEST_GROUPS = 10;
 const static int ARRAY_MAX_SIZE = 10000;
-
-// Alias
-template <typename TYPE>
-using AlgorithmMap =
-    std::unordered_map<std::string, std::function<void(std::vector<TYPE> &)>>;
-
-static std::mt19937 getRandomGenerator() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    return gen;
-}
 
 /**
  * @brief Check whether two arrays are equal by using GTest.
@@ -39,38 +27,6 @@ void checkEqualArrays(const std::vector<TYPE> & a, const std::vector<TYPE> & b,
 }
 
 /**
- * @brief Construct an algorithm map with type and compare function provided.
- *
- * @tparam TYPE Array type.
- * @param cmp Compare function.
- * @return A map contains pairs `<name, sorter>`.
- */
-template <typename TYPE, typename COMPARE = std::less<>>
-AlgorithmMap<TYPE> getAlgorithmMapByType(COMPARE cmp = std::less<>{}) {
-    // More algorithms goes here:
-    std::unordered_map<std::string, std::function<void(std::vector<TYPE> &)>>
-        result = {
-            {"Bubble Sort",
-             [cmp](std::vector<TYPE> & v) {
-                 smart_sort::bubbleSort(v.begin(), v.end(), cmp);
-             }},
-            {"Selection Sort",
-             [cmp](std::vector<TYPE> & v) {
-                 smart_sort::selectionSort(v.begin(), v.end(), cmp);
-             }},
-            {"Merge Sort",
-             [cmp](std::vector<TYPE> & v) {
-                 smart_sort::mergeSort(v.begin(), v.end(), cmp);
-             }},
-            {"Quick Sort",
-             [cmp](std::vector<TYPE> & v) {
-                 smart_sort::quickSort(v.begin(), v.end(), cmp);
-             }},
-        };
-    return result;
-}
-
-/**
  * @brief Test algorithm provided.
  *
  * @tparam TYPE Data type.
@@ -83,7 +39,7 @@ AlgorithmMap<TYPE> getAlgorithmMapByType(COMPARE cmp = std::less<>{}) {
  */
 template <typename TYPE, typename DIST, typename COMPARE = std::less<>>
 void testAlgorithm(std::mt19937 & gen, DIST & dist,
-                   const AlgorithmMap<TYPE> & algorithm_map,
+                   const smart_sort::AlgorithmMap<TYPE> & algorithm_map,
                    COMPARE cmp = std::less<>{},
                    const int array_max_size = ARRAY_MAX_SIZE) {
     std::vector<TYPE> expected;
@@ -110,9 +66,9 @@ void testAlgorithm(std::mt19937 & gen, DIST & dist,
  */
 template <typename TYPE, typename DIST_TYPE, typename COMPARE = std::less<>>
 void runTest(COMPARE cmp = std::less<>{}) {
-    std::mt19937 gen = getRandomGenerator();
+    std::mt19937 gen = smart_sort::getRandomGenerator();
     DIST_TYPE dist(LOWER_BOUNDARY, UPPER_BOUNDARY);
-    auto algorithm_map = getAlgorithmMapByType<int>(cmp);
+    auto algorithm_map = smart_sort::getAlgorithmMapByType<int>(cmp);
     testAlgorithm(gen, dist, algorithm_map, cmp);
 }
 
